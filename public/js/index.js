87ff4cceaa513c8825406f5f373da537db127310
Hubylida <hubylidayuan@outlook.com>
2017-11-19 16:37:43 +0800

    update

--- public/js/index.js
+++ public/js/index.js
@@ -2,45 +2,48 @@ $(function () {
   $("#slider").easySlider({});
 
   function actStart() {
-    var l = $('.act-content').length;
+    var l = $('.nav-circle').length;
     for (let i = 0; i < l; i++) {
       $($('.nav-circle')[i]).css('border-color', '#222');
-      $($('.act-content a')[i]).css('box-shadow','0 0 0 0');
+      $($('.act-content a')[i]).css('box-shadow', '0 0 0 0');
     }
   }
   $($('.nav-circle')[0]).css('border-color', '#e38383');
-  $($('.act-content a')[0]).css('box-shadow','0 0 15px rgba(0, 0, 0, .15), 0 6px 20px rgba(0, 0, 0, .15)');
+  $($('.act-content a')[0]).css('box-shadow', '0 0 15px rgba(0, 0, 0, .15), 0 6px 20px rgba(0, 0, 0, .15)');
+
   function actControl() {
     var actBtn = $('.nav-circle'),
       actContent = $('.act-content a');
     actBtn.map(function (i) {
-      $(actBtn[i]).on('click', function () {
+      $(actContent[i]).on('mouseover', function () {
         actStart();
-        $(actBtn[i]).css('border-color','#e38383');
-        $(actContent[i]).css('box-shadow','0 0 15px rgba(0, 0, 0, .15), 0 6px 20px rgba(0, 0, 0, .15)')
+        $(actBtn[i]).css('border-color', '#e38383');
+        $(actContent[i]).css('box-shadow', '0 0 15px rgba(0, 0, 0, .15), 0 6px 20px rgba(0, 0, 0, .15)')
       });
     });
   }
   actControl();
   var flag = true;
-  $('#nav-menu').on('click',function(){
-    if(flag){
+  $('#nav-menu').on('click', function () {
+    if (flag) {
       $('#head-nav').show();
-      $('.head').css('background','#fff');
+      $('.head').css('background', '#fff');
       flag = !flag;
-    }else{
+    } else {
       $('#head-nav').hide();
-      $('.head').css('background','rgba(255, 255, 255, .3)');
+      $('.head').css('background', 'rgba(255, 255, 255, 0)');
       flag = !flag;
     }
   })
 
-  function scroll(){
-    $(window).scroll(function(){
-      if($(window).scrollTop() > 200){
-        $('.head').css('background','#fff');
-      }else{
-        $('.head').css('background','rgba(255, 255, 255, .3)');
+  function scroll() {
+    $(window).scroll(function () {
+      if ($(window).scrollTop() > 100) {
+        $('.head').css({
+          'background': '#4e495b'
+        });
+      } else {
+        $('.head').css('background', 'rgba(255, 255, 255, 0)');
       }
     })
   }
